61add8e01642b1e95ae245c75e59d6949e0e235f
Vu Dang <danglegiavu@gmail.com>
2022-01-27 00:11:12 +0700

    implement protobuf duplex ways

--- serializer/file.go
+++ serializer/file.go
@@ -1,7 +1,54 @@
 package serializer
 
-import "github.com/golang/protobuf/proto"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/proto"
+)
 
 func WriteProtoBufferToBinaryFile(message proto.Message, filename string) error {
+	data, err := proto.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to binary: %w", err)
+	}
 
+	err = ioutil.WriteFile(filename, data, 0644)
+
+	if err != nil {
+		return fmt.Errorf("cannot write binary data to file: %w", err)
+	}
+
+	return nil
+}
+
+func ReadProtoBufferFromBinaryFile(filename string, message proto.Message) error {
+	data, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return fmt.Errorf("cannot read binary data from file: %w", err)
+	}
+
+	err = proto.Unmarshal(data, message)
+
+	if err != nil {
+		return fmt.Errorf("cannot unmarshal binary to proto message: %w", err)
+	}
+
+	return nil
+}
+
+func WriteProtoBufferToJSONFile(message proto.Message, filename string) error {
+	data, err := ProtobufToJSON(message)
+
+	if err != nil {
+		return fmt.Errorf("cannot marshal proto message to JSON: %w", err)
+	}
+
+	err = ioutil.WriteFile(filename, []byte(data), 0644)
+
+	if err != nil {
+		return fmt.Errorf("cannot write JSON Data to file: %w", err)
+	}
+	return nil
 }
