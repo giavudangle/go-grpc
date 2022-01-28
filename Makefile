02fc0f0f28d126e0d5a1f5e89e989d7c87b3d0a7
Vu Dang <danglegiavu@gmail.com>
2022-01-29 00:54:23 +0700

    add main func

---
+++ Makefile
@@ -0,0 +1,11 @@
+gen:
+	protoc --proto_path=proto proto/*.proto  --go_out=:pb --go-grpc_out=:pb
+clean:
+	rm pb/*.go
+server:
+	go run cmd/server/main.go -port 8080
+client:
+	go run cmd/client/main.go
+test:
+	go test -cover -race ./...
+
---
+++ cmd/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/giavudangle/go-grpc/pb"
+	"github.com/giavudangle/go-grpc/service"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+	port := flag.Int("port", 0, "the server of port")
+
+	flag.Parse()
+
+	log.Printf("start server on port %d", *port)
+
+	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	address := fmt.Sprintf("0.0.0.0:%d", *port)
+	listener, err := net.Listen("tcp", address)
+
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+
+	err = grpcServer.Serve(listener)
+
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+
+}
